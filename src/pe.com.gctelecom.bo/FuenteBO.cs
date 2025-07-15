ba5d3364dba81601e391567eccad4509eb021d7e
Francisco Huapaya <franciscojho@hotmail.com>
2025-07-14 22:42:13 -0500

    feat: anadir clases bo

---
+++ src/pe.com.gctelecom.bo/FuenteBO.cs
@@ -0,0 +1,10 @@
+﻿
+namespace pe.com.gctelecom.bo
+{
+    public class FuenteBO
+    {
+        public int FuenteId { get; set; }
+        public int Nombre { get; set; }
+        public int EsVisible { get; set; }
+    }
+}
---
+++ src/pe.com.gctelecom.bo/VendedorBO.cs
@@ -0,0 +1,12 @@
+﻿
+namespace pe.com.gctelecom.bo
+{
+    public class VendedorBO
+    {
+        public int VendedorId { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public bool EsVisible { get; set; }
+    }
+}
