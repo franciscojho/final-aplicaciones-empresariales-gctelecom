a8274c19a559fd16404e19fc2f42f1ea98821546
Francisco Huapaya <franciscojho@hotmail.com>
2025-07-18 22:05:43 -0500

    feat: anadir bal clases

---
+++ src/pe.com.gctelecom.bal/FuenteBAL.cs
@@ -0,0 +1,45 @@
+﻿using pe.com.gctelecom.bo;
+using pe.com.gctelecom.dal;
+
+namespace pe.com.gctelecom.bal
+{
+    public class FuenteBAL
+    {
+        private FuenteDAL FuenteDAL = new FuenteDAL();
+
+        public List<FuenteBO> ObtenerFuentesHabilitados()
+        {
+            return FuenteDAL.ObtenerFuentes(true);
+        }
+
+        public List<FuenteBO> ObtenerTodosFuentes()
+        {
+            return FuenteDAL.ObtenerFuentes();
+        }
+
+        public bool CrearFuente(FuenteBO Fuente)
+        {
+            return FuenteDAL.CrearFuente(Fuente);
+        }
+
+        public bool ActualizarFuente(FuenteBO Fuente)
+        {
+            return FuenteDAL.ActualizarFuente(Fuente);
+        }
+
+        public bool BorrarFuente(int FuenteId)
+        {
+            return FuenteDAL.ActivarDesactivarFuente(FuenteId, false);
+        }
+
+        public bool HabilitarFuente(int FuenteId)
+        {
+            return FuenteDAL.ActivarDesactivarFuente(FuenteId, true);
+        }
+
+        public int ObtenerSiguienteFuenteId()
+        {
+            return FuenteDAL.ObtenerSiguienteFuenteId();
+        }
+    }
+}
---
+++ src/pe.com.gctelecom.bal/VendedorBAL.cs
@@ -0,0 +1,45 @@
+﻿using pe.com.gctelecom.bo;
+using pe.com.gctelecom.dal;
+
+namespace pe.com.gctelecom.bal
+{
+    public class VendedorBAL
+    {
+        private VendedorDAL VendedorDAL = new VendedorDAL();
+
+        public List<VendedorBO> ObtenerVendedorsHabilitados()
+        {
+            return VendedorDAL.ObtenerVendedores(true);
+        }
+
+        public List<VendedorBO> ObtenerTodosVendedors()
+        {
+            return VendedorDAL.ObtenerVendedores();
+        }
+
+        public bool CrearVendedor(VendedorBO Vendedor)
+        {
+            return VendedorDAL.CrearVendedor(Vendedor);
+        }
+
+        public bool ActualizarVendedor(VendedorBO Vendedor)
+        {
+            return VendedorDAL.ActualizarVendedor(Vendedor);
+        }
+
+        public bool BorrarVendedor(int VendedorId)
+        {
+            return VendedorDAL.ActivarDesactivarVendedor(VendedorId, false);
+        }
+
+        public bool HabilitarVendedor(int VendedorId)
+        {
+            return VendedorDAL.ActivarDesactivarVendedor(VendedorId, true);
+        }
+
+        public int ObtenerSiguienteVendedorId()
+        {
+            return VendedorDAL.ObtenerSiguienteVendedorId();
+        }
+    }
+}
